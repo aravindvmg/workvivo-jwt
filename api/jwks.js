05dc1b0eeff48b5d757966b32220a39f454095e4
Aravindkumar <aravindkumar@vmgdigital.com>
2025-04-23 18:32:26 +0530

    updated jwks

--- api/jwks.js
+++ api/jwks.js
@@ -1,9 +1,24 @@
-import jwks from '../jwks.json';
+// import jwks from '../jwks.json';
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(jwks);
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
+// export default function handler(req, res) {
+//   if (req.method === 'GET') {
+//     res.status(200).json(jwks);
+//   } else {
+//     res.status(405).json({ message: 'Method Not Allowed' });
+//   }
+// }
+
+
+// import { generateJwks } from '../generate-jwks.js';
+
+// export default function handler(req, res) {
+//   const jwks = generateJwks(); // or await if it's async
+//   res.status(200).json(jwks);
+// }
+
+import { generateJwks } from '../scripts/generate-jwks.js';
+
+export default async function handler(req, res) {
+  const jwks = await generateJwks();
+  res.status(200).json(jwks);
 }
--- scripts/generate-jwks.js
+++ scripts/generate-jwks.js
@@ -4,16 +4,11 @@ import { exportJWK } from 'jose/key/export';
 
 const pubKey = fs.readFileSync('./public.pem', 'utf8');
 
-const generateJWKS = async () => {
+export async function generateJwks() {
   const keyObj = await importSPKI(pubKey, 'RS256');
   const jwk = await exportJWK(keyObj);
   jwk.kid = '300|9925f2248d89c4bafba385aeecd80f9908ccfd0b';
   jwk.use = 'sig';
   jwk.alg = 'RS256';
-
-  const jwks = { keys: [jwk] };
-  fs.writeFileSync('jwks.json', JSON.stringify(jwks, null, 2));
-  console.log('✅ JWKS saved to jwks.json');
-};
-
-generateJWKS();
+  return { keys: [jwk] };
+}
