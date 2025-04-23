ec737d39558d7512e21b2b0b37737af33424c2dc
Aravindkumar <aravindkumar@vmgdigital.com>
2025-04-23 15:32:02 +0530

    change code

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-// import express from 'express';
-// import fs from 'fs';
-
-// const app = express();
-// const port = 3000;
-
-// const jwks = JSON.parse(fs.readFileSync('./jwks.json', 'utf8'));
-
-// app.get('/.well-known/jwks.json', (req, res) => {
-//   res.json(jwks);
-// });
-
-// app.listen(port, () => {
-//   console.log(`✅ JWKS running: http://localhost:${port}/.well-known/jwks.json`);
-// });
-
-// api/jwks.js
-
+import express from 'express';
 import fs from 'fs';
+
+const app = express();
+const port = 3000;
 
 const jwks = JSON.parse(fs.readFileSync('./jwks.json', 'utf8'));
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(jwks);
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
+app.get('/.well-known/jwks.json', (req, res) => {
+  res.json(jwks);
+});
+
+app.listen(port, () => {
+  console.log(`✅ JWKS running: http://localhost:${port}/.well-known/jwks.json`);
+});
+
+// api/jwks.js
+
+// import fs from 'fs';
+
+// const jwks = JSON.parse(fs.readFileSync('./jwks.json', 'utf8'));
+
+// export default function handler(req, res) {
+//   if (req.method === 'GET') {
+//     res.status(200).json(jwks);
+//   } else {
+//     res.status(405).json({ message: 'Method Not Allowed' });
+//   }
+// }
 
